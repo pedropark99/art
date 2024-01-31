ccd0e266da14ae0c8c193adbd51aa7aa8c0a3b2b
pedropark99 <pedropark99@gmail.com>
2024-01-31 13:37:23 -0300

    Add files

--- art-lines.R
+++ art-lines.R
@@ -11,8 +11,8 @@ deg2rad <- function(deg) {
   (deg * pi) / 180
 }
 
-width <- 1000
-height <- 1000
+width <- 2000
+height <- 2000
 
 step <- 10
 x <- 20
@@ -25,7 +25,7 @@ df <- tibble(
   n_step = seq_len(n_steps),
   x = ((n_step - 1) * step) + x,
   angle = deg2rad(n_step),
-  y = 50 + sin(angle * 5)
+  y = 50 + sin(angle * 2)
 )
 
 
@@ -33,3 +33,21 @@ ggplot(df) +
   geom_line(
     aes(group = 1, x = x, y = y)
   )
+
+
+
+
+noises <- gen_perlin(1, y = 1 + (seq_len(n_steps) * 0.1))
+df <- tibble(
+  n_step = seq_len(n_steps),
+  x = ((n_step - 1) * step) + x,
+  angle = deg2rad(n_step),
+  y = seq_len(n_steps) * 2 + 50 + (noises * 50)
+)
+
+ggplot(df) +
+  geom_line(
+    aes(group = 1, x = x, y = y)
+  ) +
+  xlim(c(0, width)) +
+  ylim(c(0, height))
