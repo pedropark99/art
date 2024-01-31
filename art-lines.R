b129721a47e36c1037c205bb91ef5654f5472302
pedropark99 <pedropark99@gmail.com>
2024-01-31 12:38:46 -0300

    Initial commit

---
+++ art-lines.R
@@ -0,0 +1,35 @@
+setwd("C:/Users/pedro/Documents/Projetos/Livros/art")
+
+library(tidyverse)
+library(ambient)
+
+rad2deg <- function(rad) {
+  (rad * 180) / pi
+}
+
+deg2rad <- function(deg) {
+  (deg * pi) / 180
+}
+
+width <- 1000
+height <- 1000
+
+step <- 10
+x <- 20
+y <- 50
+lastx <- x
+lasty <- y
+
+n_steps <- ((width - 200) - x) / step
+df <- tibble(
+  n_step = seq_len(n_steps),
+  x = ((n_step - 1) * step) + x,
+  angle = deg2rad(n_step),
+  y = 50 + sin(angle * 5)
+)
+
+
+ggplot(df) +
+  geom_line(
+    aes(group = 1, x = x, y = y)
+  )
