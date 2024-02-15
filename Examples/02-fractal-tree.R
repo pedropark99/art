c089b2f87bd9f76e06c628d7d068af82012cb9a5
pedropark99 <pedropark99@gmail.com>
2024-02-14 21:44:18 -0300

    Add exampe

--- Examples/02-fractal-tree.R
+++ Examples/02-fractal-tree.R
@@ -12,48 +12,44 @@ trunk <- data.frame(
 
 x_start <- 0
 y_start <- 150
+coord_index <- 1
 coords <- list(
-  x = vector("double", 100),
-  y = vector("double", 100)
+  x = vector("double", 1000),
+  y = vector("double", 1000)
 )
 
 
-branch <- function(x, y, a, len, coords, index) {
-  if (len < 5) {
-    return(coords)
+branch <- function(x, y, a, len) {
+  if (len > 5) {
+    new_len <- len * 0.67
+    
+    new_angle <- a + deg2rad(15)
+    new_x <- (len * cos(new_angle)) + x
+    new_y <- (len * sin(new_angle)) + y
+    coords$x[coord_index] <<- new_x
+    coords$y[coord_index] <<- new_y
+    coord_index <<- coord_index + 1
+    
+    branch(new_x, new_y, new_angle, new_len)
+    
+    new_angle <- a - deg2rad(15)
+    new_x <- (len * cos(new_angle)) + x
+    new_y <- (len * sin(new_angle)) + y
+    coords$x[coord_index] <<- new_x
+    coords$y[coord_index] <<- new_y
+    coord_index <<- coord_index + 1
+    
+    branch(new_x, new_y, new_angle, new_len)
   }
-  
-  rotate_factor <- deg2rad(15)
-  new_len <- len - 15
-  
-  # The left side
-  new_angle <- a + rotate_factor
-  new_x <- (len * cos(new_angle)) + x
-  new_y <- (len * sin(new_angle)) + y
-  
-  coords$x[index] <- new_x
-  coords$y[index] <- new_y
-  index <- index + 1
-  
-  branch(new_x, new_y, new_angle, new_len, coords, index)
-  
-  # The right side
-  new_angle <- a - rotate_factor
-  new_x <- (len * cos(new_angle)) + x
-  new_y <- (len * sin(new_angle)) + y
-  
-  coords$x[index] <- new_x
-  coords$y[index] <- new_y
-  index <- index + 1
-  
-  branch(new_x, new_y, new_angle, new_len, coords, index)
+
+  return()
 }
 
 
 
 
-t <- branch(x_start, y_start, deg2rad(90), 100, coords, 1)
-df <- as_tibble(t) |>
+branch(0, 150, deg2rad(90), 150)
+df <- as_tibble(coords) |>
   filter(x != 0)
 
 
