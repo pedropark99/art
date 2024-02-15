904f6e36d4ad9b61f88e1b1b76b58ec2ad23bd1a
pedropark99 <pedropark99@gmail.com>
2024-02-15 17:16:55 -0300

    Add working version of fractal tree

--- Examples/02-fractal-tree.R
+++ Examples/02-fractal-tree.R
@@ -1,59 +1,73 @@
 library(tidyverse)
 
+# Function to convert degrees into radians
 deg2rad <- function(deg){
   return(deg * pi / 180)
 }
 
 
-trunk <- data.frame(
-  x = c(0, 0),
-  y = c(0, 150)
-)
-
-x_start <- 0
-y_start <- 150
-coord_index <- 1
-coords <- list(
-  x = vector("double", 1000),
-  y = vector("double", 1000)
-)
-
-
-branch <- function(x, y, a, len) {
-  if (len > 5) {
-    new_len <- len * 0.67
-    
-    new_angle <- a + deg2rad(15)
-    new_x <- (len * cos(new_angle)) + x
-    new_y <- (len * sin(new_angle)) + y
-    coords$x[coord_index] <<- new_x
-    coords$y[coord_index] <<- new_y
-    coord_index <<- coord_index + 1
-    
-    branch(new_x, new_y, new_angle, new_len)
-    
-    new_angle <- a - deg2rad(15)
-    new_x <- (len * cos(new_angle)) + x
-    new_y <- (len * sin(new_angle)) + y
-    coords$x[coord_index] <<- new_x
-    coords$y[coord_index] <<- new_y
-    coord_index <<- coord_index + 1
-    
-    branch(new_x, new_y, new_angle, new_len)
+fractal_tree <- function(x_start, y_start, len_start, rotate_angle, ldf) {
+  rotate_factor <- deg2rad(rotate_angle)
+  length_decrease_factor <- ldf
+  coord_index <- 1
+  coords <- list(
+    x = rep(NA_real_, 8000),
+    y = rep(NA_real_, 8000)
+  )
+  
+  branch <- function(x, y, a, len) {
+    if (len > 5) {
+      new_len <- len * length_decrease_factor
+      
+      new_angle <- a + rotate_factor
+      new_x <- (len * cos(new_angle)) + x
+      new_y <- (len * sin(new_angle)) + y
+      
+      i <- coord_index:(coord_index + 3)
+      coords$x[i] <<- c(0 + x, new_x)
+      coords$y[i] <<- c(0 + y, new_y)
+      coord_index <<- coord_index + 4
+      
+      branch(new_x, new_y, new_angle, new_len)
+      
+      
+      
+      new_angle <- a - rotate_factor
+      new_x <- (len * cos(new_angle)) + x
+      new_y <- (len * sin(new_angle)) + y
+      
+      i <- coord_index:(coord_index + 3)
+      coords$x[i] <<- c(0 + x, new_x)
+      coords$y[i] <<- c(0 + y, new_y)
+      coord_index <<- coord_index + 4
+      
+      branch(new_x, new_y, new_angle, new_len)
+    }
+  
+    return()
   }
-
-  return()
+  
+  branch(x_start, y_start, deg2rad(90), len_start * 0.67)
+  trunk <- data.frame(
+    x = c(x_start, x_start),
+    y = c(y_start, y_start - len_start)
+  )
+  
+  coords <- as_tibble(coords) %>%
+    filter(!is.na(x)) %>% 
+    bind_rows(trunk)
+  
+  coords <- coords %>% 
+    mutate(
+      line_id = rep(seq_len(nrow(coords) / 2), each = 2)
+    )
+    
+  return(coords)
 }
 
-
-
-
-branch(0, 150, deg2rad(90), 150)
-df <- as_tibble(coords) |>
-  filter(x != 0)
-
-
+df <- fractal_tree(0, 150, 120, 15, 0.75)
 
 df %>% 
   ggplot() +
-  geom_point(aes(x, y))
+  geom_path(aes(x, y, group = line_id)) +
+  theme_void()
