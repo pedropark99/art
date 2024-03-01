a9f3f6cbfc83b74d7ad61109cee61f85109a3f3e
pedropark99 <pedropark99@gmail.com>
2024-03-01 19:44:40 -0300

    Add content

--- Examples/04-marching-squares.R
+++ Examples/04-marching-squares.R
@@ -7,6 +7,8 @@ grid_height <- n
 grid_width <- n
 set.seed(50)
 values <- noise_simplex(c(n, n))
+values <- values[55:65,50:60]
+n <- 11
 binary_grid <- values > 0
 
 build_grid_df <- function(angles, n) {
@@ -27,6 +29,7 @@ get_cell <- function(x, y) {
 
 S7_polygon <- new_class("S7_polygon",
   properties = list(
+    case_index = class_integer,
     x = class_double,
     y = class_double
   )
@@ -51,22 +54,22 @@ da <- c(0, 0.5)
 
 
 cases <- list(
-  S7_polygon(x = 0, y = 0),
-  S7_polygon(x = c(d[1], cd[1], da[1]), y = c(d[2], cd[2], da[2])),
-  S7_polygon(x = c(c[1], cd[1], bc[1]), y = c(c[2], cd[2], bc[2])),
-  S7_polygon(x = c(d[1], c[1], bc[1], da[1]), y = c(d[2], c[2], bc[2], da[2])),
-  S7_polygon(x = c(b[1], ab[1], bc[1]), y = c(b[2], ab[2], bc[2])),
-  S7_polygon(x = c(d[1], da[1], ab[1], b[1], bc[1], cd[1]), y = c(d[2], da[2], ab[2], b[2], bc[2], cd[2])),
-  S7_polygon(x = c(ab[1], b[1], c[1], cd[1]), y = c(ab[2], b[2], c[2], cd[2])),
-  S7_polygon(x = c(da[1], d[1], c[1], b[1], ab[1]), y = c(da[2], d[2], c[2], b[2], ab[2])),
-  S7_polygon(x = c(a[1], ab[1], da[1]), y = c(a[2], ab[2], da[2])),
-  S7_polygon(x = c(a[1], ab[1], cd[1], d[1]), y = c(a[2], ab[2], cd[2], d[2])),
-  S7_polygon(x = c(a[1], ab[1], bc[1], c[1], cd[1], da[1]), y = c(a[2], ab[2], bc[2], c[2], cd[2], da[2])),
-  S7_polygon(x = c(a[1], ab[1], bc[1], c[1], d[1]), y = c(a[2], ab[2], bc[2], c[2], d[2])),
-  S7_polygon(x = c(a[1], b[1], bc[1], da[1]), y = c(a[2], b[2], bc[2], da[2])),
-  S7_polygon(x = c(a[1], b[1], bc[1], cd[1], d[1]), y = c(a[2], b[2], bc[2], cd[2], d[2])),
-  S7_polygon(x = c(a[1], b[1], c[1], cd[1], da[1]), y = c(a[2], b[2], c[2], cd[2], da[2])),
-  S7_polygon(x = c(a[1], b[1], c[1], d[1]), y = c(a[2], b[2], c[2], d[2]))
+  S7_polygon(case_index = 0L, x = 0, y = 0),
+  S7_polygon(case_index = 1L, x = c(d[1], cd[1], da[1]), y = c(d[2], cd[2], da[2])),
+  S7_polygon(case_index = 2L, x = c(c[1], cd[1], bc[1]), y = c(c[2], cd[2], bc[2])),
+  S7_polygon(case_index = 3L, x = c(d[1], c[1], bc[1], da[1]), y = c(d[2], c[2], bc[2], da[2])),
+  S7_polygon(case_index = 4L, x = c(b[1], ab[1], bc[1]), y = c(b[2], ab[2], bc[2])),
+  S7_polygon(case_index = 5L, x = c(d[1], da[1], ab[1], b[1], bc[1], cd[1]), y = c(d[2], da[2], ab[2], b[2], bc[2], cd[2])),
+  S7_polygon(case_index = 6L, x = c(ab[1], b[1], c[1], cd[1]), y = c(ab[2], b[2], c[2], cd[2])),
+  S7_polygon(case_index = 7L, x = c(da[1], d[1], c[1], b[1], ab[1]), y = c(da[2], d[2], c[2], b[2], ab[2])),
+  S7_polygon(case_index = 8L, x = c(a[1], ab[1], da[1]), y = c(a[2], ab[2], da[2])),
+  S7_polygon(case_index = 9L, x = c(a[1], ab[1], cd[1], d[1]), y = c(a[2], ab[2], cd[2], d[2])),
+  S7_polygon(case_index = 10L, x = c(a[1], ab[1], bc[1], c[1], cd[1], da[1]), y = c(a[2], ab[2], bc[2], c[2], cd[2], da[2])),
+  S7_polygon(case_index = 11L, x = c(a[1], ab[1], bc[1], c[1], d[1]), y = c(a[2], ab[2], bc[2], c[2], d[2])),
+  S7_polygon(case_index = 12L, x = c(a[1], b[1], bc[1], da[1]), y = c(a[2], b[2], bc[2], da[2])),
+  S7_polygon(case_index = 13L, x = c(a[1], b[1], bc[1], cd[1], d[1]), y = c(a[2], b[2], bc[2], cd[2], d[2])),
+  S7_polygon(case_index = 14L, x = c(a[1], b[1], c[1], cd[1], da[1]), y = c(a[2], b[2], c[2], cd[2], da[2])),
+  S7_polygon(case_index = 15L, x = c(a[1], b[1], c[1], d[1]), y = c(a[2], b[2], c[2], d[2]))
 )
 
 get_polygon <- function(case_index) {
@@ -102,35 +105,91 @@ for (x in seq_len(nrow(binary_grid))) {
   }
 }
 
+
+flip_vertically_polygon <- function(polygon) {
+  my <- max(polygon@y)
+  S7_polygon(
+    case_index = polygon@case_index,
+    x = polygon@x,
+    y = my - polygon@y
+  )
+}
+
+
 result <- tibble(
   polygons = polygons[polygons_indexes != 0L],
   p_index = polygons_indexes[polygons_indexes != 0L],
   grid_x = grid_x[grid_x != 0L],
   grid_y = grid_y[grid_y != 0L]
 ) %>%
+  # mutate(
+  #   polygons = map(polygons, flip_vertically_polygon)
+  # ) %>%
   mutate(
+    case_index = map_int(polygons, \(p) p@case_index),
     p_x = map2(polygons, grid_x, \(p, grid_x) p@x + grid_x),
-    p_y = map2(polygons, grid_y, \(p, grid_y) p@y + grid_y)
+    p_y = map2(polygons, grid_y, \(p, grid_y) p@y + grid_y),
   ) %>% 
   unnest_longer(c(p_x, p_y))
 
-result
+result %>% 
+  filter(case_index == 9)
 
 
 
 result %>% 
+  filter(case_index == 9) %>% 
   ggplot() +
-  geom_point(
-    aes(x, y),
-    data = grid_as_df %>% filter(value > 0)
-  ) +
   geom_polygon(
     aes(x = p_x, y = p_y, group = p_index),
     fill = "blue"
+  ) + 
+  geom_point(
+    aes(x, y, color = value > 0),
+    data = grid_as_df,
+    size = 1
   ) +
   theme_void()
 
 
 
+cases_df <- tibble(
+  case_index = seq_along(cases),
+  case = cases
+) %>% 
+  mutate(
+    p_x = map(case, \(p) p@x),
+    p_y = map(case, \(p) p@y)
+  ) %>% 
+  unnest_longer(c(p_x, p_y))
+
+
+cases_df %>% 
+  filter(case_index == 16) %>% 
+ggplot() +
+  geom_polygon(
+    aes(x = x, y = y),
+    fill = "black",
+    data = data.frame(x = c(0, 0, 1, 1), y = c(0, 1, 1, 0))
+  ) +
+  geom_polygon(
+    aes(
+      x = p_x, y = p_y,
+      fill = as.factor(case_index),
+      group = as.factor(case_index)
+    )
+  ) +
+  coord_cartesian(
+    ylim = c(-2, 2),
+    xlim = c(-2, 2)
+  ) +
+  theme(
+    legend.position = "bottom"
+  )
+  
+
+
+
+flip_vertically_polygon(result$polygons[[1]])
 
 
